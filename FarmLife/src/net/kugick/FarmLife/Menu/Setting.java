d8928d8dffed3d86769f00c8b2d3b6e1a67db360
kugick <contact@kugick.net>
2016-10-31 21:39:03 -0400

    New files

--- FarmLife/src/net/kugick/FarmLife/Menu/Setting.java
+++ FarmLife/src/net/kugick/FarmLife/Menu/Setting.java
@@ -1,5 +1,5 @@
 package net.kugick.FarmLife.Menu;
 
-public class Settings{
+public class Setting{
 
 }
--- FarmLife/src/net/kugick/FarmLife/Menu/Shop.java
+++ FarmLife/src/net/kugick/FarmLife/Menu/Shop.java
@@ -1,5 +1,5 @@
 package net.kugick.FarmLife.Menu;
 
-public class Achievement{
+public class Shop{
 
 }
