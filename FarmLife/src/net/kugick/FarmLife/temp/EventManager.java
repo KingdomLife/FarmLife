5cc38113cbb63d7290c108d5573c45652687850e
sebcio98 <NightmareDARQ@gmail.com>
2016-12-27 12:59:26 +0100

    Create EventManager.java

---
+++ FarmLife/src/net/kugick/FarmLife/temp/EventManager.java
@@ -0,0 +1,17 @@
+package net.kugick.FarmLife.temp;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class EventManager implements Listener
+{
+	@EventHandler(priority=EventPriority.LOW)
+	public void onPlayerJoin(PlayerJoinEvent e)
+		{FLDatabase.createTempFile(e.getPlayer().getUniqueId());}
+	@EventHandler(priority=EventPriority.LOW)
+	public void onPlayerQuit(PlayerQuitEvent e)
+		{FLDatabase.deleteTempFile(e.getPlayer().getUniqueId());}
+}
