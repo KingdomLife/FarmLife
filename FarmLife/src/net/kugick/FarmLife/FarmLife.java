d7f072c940a7106ca752638b68da9de66b23e8a4
sebcio98 <NightmareDARQ@gmail.com>
2016-12-27 17:29:28 +0100

    Update and rename Main.java to FarmLife.java

---
+++ FarmLife/src/net/kugick/FarmLife/FarmLife.java
@@ -0,0 +1,28 @@
+package net.kugick.FarmLife;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import net.kugick.FarmLife.WorldGenerator.FarmGenerator;
+
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FarmLife extends JavaPlugin
+{
+	private static FarmLife instance;
+	
+	public static FarmLife getInstance()
+		{return instance;}
+	public void onEnable()
+	{
+		instance = this;
+		
+	}
+	
+	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
+	{
+		File farmSchematicFile = Paths.get(getDataFolder().toPath().toString(), "Farm.schematic").toFile();
+		return farmSchematicFile == null ? null : new FarmGenerator(farmSchematicFile);
+	}
+}
