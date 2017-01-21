af11a7553aa3c6519ad0e99e4d442bcadb859791
sebcio98 <NightmareDARQ@gmail.com>
2017-01-21 14:04:25 +0100

    Create Utils.java

---
+++ FarmLife/src/net/kugick/FarmLife/Util/Utils.java
@@ -0,0 +1,11 @@
+package net.kugick.FarmLife.Util;
+
+public class Utils
+{
+	//////////////////
+	// String utils //
+	//////////////////
+	public static String wrap(String inp, String paper)
+		{return paper + inp + paper;}
+	
+}
