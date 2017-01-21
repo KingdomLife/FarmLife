35e308130e3c589feecc47f5af95c59ebfcfc3fc
sebcio98 <NightmareDARQ@gmail.com>
2017-01-21 14:02:18 +0100

    Update Config.java

--- FarmLife/src/net/kugick/FarmLife/Config/Config.java
+++ FarmLife/src/net/kugick/FarmLife/Config/Config.java
@@ -3,28 +3,13 @@ package net.kugick.FarmLife.Config;
 public class Config
 {
 	//Temporary config values, move to config file (maybe?)
-	//btw, this kind of setup is annoying, needs to be changed
 	//TODO fill in the user and password
-	public static enum DataType {db_user, db_password, db_ip, db_database, db_table};
+	public static ConfigDB DB = new ConfigDB("localhost", "SA", "", "FarmLife", "playerdata", "grandmarket");
 	
-	private final static String db_user = "test", db_password = "test", db_ip = "127.0.0.1",
-						db_table = "playerdata", db_database = "FarmLife";
-	public static String getData(DataType type)
+	public static void reload()
 	{
-		switch (type)
-		{
-			case db_user:
-				return db_user;
-			case db_password:
-				return db_password;
-			case db_ip:
-				return db_ip;
-			case db_database:
-				return db_database;
-			case db_table:
-				return db_table;
-			default: return null;
-		}
+		//TODO reload from file (? ^)
+		
+		//DB = new ConfigSQL(file -> values);
 	}
-	
 }
