04ae79249895cb7c319c71888417a3c0ffae4962
sebcio98 <NightmareDARQ@gmail.com>
2017-01-27 08:38:33 +0100

    Some fixes from Seb, because it baisically didn't work

--- FarmLife/src/net/kugick/FarmLife/Event/Chat.java
+++ FarmLife/src/net/kugick/FarmLife/Event/Chat.java
@@ -1,6 +1,5 @@
 package net.kugick.FarmLife.Event;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -14,21 +13,16 @@ public class Chat implements Listener{
 	public void onChat(AsyncPlayerChatEvent e){
 		org.bukkit.entity.Player p = e.getPlayer();
 
-		e.setCancelled(true);
-
 		String prefix = null;
 		String level = null;
-
-		for(org.bukkit.entity.Player online : Bukkit.getOnlinePlayers()){
-			//is he in staff
-			if(p.hasPermission("FarmLife.staff")){
-				e.setMessage("<"+prefix+"> " + p.getName() + ": " + e.getMessage());
-			}
-			//normal player
-			else{
-				//Just default preset of the chat, still need to do colors for ranks
-				e.setMessage("["+level+"] " + p.getName() + ": " + e.getMessage());
-			}
+		//is he in staff
+		if(p.hasPermission("FarmLife.staff")){
+			e.setMessage("<"+prefix+"> " + p.getName() + ": " + e.getMessage());
+		}
+		//normal player
+		else{
+			//Just default preset of the chat, still need to do colors for ranks
+			e.setMessage("["+level+"] " + p.getName() + ": " + e.getMessage());
 		}
 	}
 }
