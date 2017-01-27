0a3200a202d9e5df3512457e95ac40dd17994ceb
kugick <contact@kugick.net>
2017-01-27 06:34:56 -0500

    Revert "Some fixes from Seb, because it baisically didn't work"
    This reverts commit 04ae79249895cb7c319c71888417a3c0ffae4962.

--- FarmLife/src/net/kugick/FarmLife/Event/Chat.java
+++ FarmLife/src/net/kugick/FarmLife/Event/Chat.java
@@ -1,5 +1,6 @@
 package net.kugick.FarmLife.Event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -13,16 +14,21 @@ public class Chat implements Listener{
 	public void onChat(AsyncPlayerChatEvent e){
 		org.bukkit.entity.Player p = e.getPlayer();
 
+		e.setCancelled(true);
+
 		String prefix = null;
 		String level = null;
-		//is he in staff
-		if(p.hasPermission("FarmLife.staff")){
-			e.setMessage("<"+prefix+"> " + p.getName() + ": " + e.getMessage());
-		}
-		//normal player
-		else{
-			//Just default preset of the chat, still need to do colors for ranks
-			e.setMessage("["+level+"] " + p.getName() + ": " + e.getMessage());
+
+		for(org.bukkit.entity.Player online : Bukkit.getOnlinePlayers()){
+			//is he in staff
+			if(p.hasPermission("FarmLife.staff")){
+				e.setMessage("<"+prefix+"> " + p.getName() + ": " + e.getMessage());
+			}
+			//normal player
+			else{
+				//Just default preset of the chat, still need to do colors for ranks
+				e.setMessage("["+level+"] " + p.getName() + ": " + e.getMessage());
+			}
 		}
 	}
 }
